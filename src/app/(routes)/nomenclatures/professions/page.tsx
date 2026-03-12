e5f2aabd6b177db8f02cc5ab272b458fd0b5ebcd
raducubalgiu <balgiu_radu_2007@yahoo.com>
2026-03-12 08:59:45 +0200

    Clean Professions page

--- src/app/(routes)/nomenclatures/professions/page.tsx
+++ src/app/(routes)/nomenclatures/professions/page.tsx
@@ -1,17 +1,9 @@
 import { ProtectedPage } from "@/components/cutomized/Protected/ProtectedPage";
 import ProfessionsModule from "@/components/modules/Nomenclatures/ProfessionsModule/ProfessionsModule";
-import { BusinessDomainType } from "@/ts/models/nomenclatures/BusinessDomainType";
-import { get } from "@/utils/requests";
 import React from "react";
 
 async function Professions() {
-	const businessDomains = (
-		await get<BusinessDomainType[]>({
-			url: `/business-domains`,
-		})
-	).data;
-
-	return <ProfessionsModule businessDomains={businessDomains} />;
+  return <ProfessionsModule />;
 }
 
 export default ProtectedPage(Professions, "NOMENCLATURES_VIEW");
--- src/components/modules/Nomenclatures/ProfessionsModule/ProfessionsModule.tsx
+++ src/components/modules/Nomenclatures/ProfessionsModule/ProfessionsModule.tsx
@@ -7,99 +7,64 @@ import { MRT_ColumnDef } from "material-react-table";
 import { useMemo } from "react";
 import MR_Input from "@/components/core/Table/MR_Inputs/MR_Input";
 import { ProfessionType } from "@/ts/models/Profession/ProfessionType";
-import { BusinessDomainType } from "@/ts/models/nomenclatures/BusinessDomainType";
-import MR_Select from "@/components/core/Table/MR_Inputs/MR_Select";
-import ProfessionBusinessTypes from "./ProfessionBusinessTypes";
 
-type ProfessionsModuleProps = { businessDomains: BusinessDomainType[] };
+export default function ProfessionsModule() {
+  const {
+    data,
+    isLoading,
+    onCreatingRowSave,
+    pagination,
+    setPagination,
+    onEditingRowSave,
+    onDeletingRowSave,
+  } = useTableHandlers<ProfessionType>({
+    route: "nomenclatures/professions",
+  });
 
-export default function ProfessionsModule({
-	businessDomains,
-}: ProfessionsModuleProps) {
-	const {
-		data,
-		isLoading,
-		onCreatingRowSave,
-		pagination,
-		setPagination,
-		onEditingRowSave,
-		onDeletingRowSave,
-	} = useTableHandlers<ProfessionType>({
-		route: "nomenclatures/professions",
-	});
+  const columns = useMemo<MRT_ColumnDef<ProfessionType>[]>(
+    () => [
+      {
+        accessorKey: "id",
+        header: "ID",
+        size: 50,
+        enableEditing: false,
+      },
+      {
+        accessorKey: "name",
+        header: "Name",
+        Edit: ({ row, column }) => (
+          <MR_Input
+            row={row}
+            column={column}
+            value={row.original.name}
+            required
+            minLength={3}
+            maxLength={100}
+          />
+        ),
+      },
+      {
+        accessorKey: "created_at",
+        header: "Created_at",
+        enableEditing: false,
+      },
+    ],
+    []
+  );
 
-	const columns = useMemo<MRT_ColumnDef<ProfessionType>[]>(
-		() => [
-			{
-				accessorKey: "id",
-				header: "ID",
-				size: 50,
-				enableEditing: false,
-			},
-			{
-				accessorKey: "name",
-				header: "Name",
-				Edit: ({ row, column }) => (
-					<MR_Input
-						row={row}
-						column={column}
-						value={row.original.name}
-						required
-						minLength={3}
-						maxLength={100}
-					/>
-				),
-			},
-			{
-				accessorKey: "business_domain_id",
-				header: "Business Domain Id",
-				Edit: ({ row, column, cell }) => (
-					<MR_Select
-						row={row}
-						column={column}
-						value={Number(cell.getValue()) ?? ""}
-						options={businessDomains.map(bd => {
-							return {
-								value: bd.id,
-								name: bd.name,
-							};
-						})}
-					/>
-				),
-				Cell: ({ cell }) =>
-					businessDomains?.find(bd => bd.id === cell.getValue())?.name,
-			},
-			{
-				accessorKey: "created_at",
-				header: "Created_at",
-				enableEditing: false,
-			},
-		],
-		[businessDomains]
-	);
-
-	return (
-		<MainLayout title="Profesii" hideAction>
-			<Table<ProfessionType>
-				data={data?.results}
-				rowCount={data?.count}
-				columns={columns}
-				manualPagination
-				onCreatingRowSave={onCreatingRowSave}
-				onEditingRowSave={onEditingRowSave}
-				onDeletingRowSave={onDeletingRowSave}
-				onPaginationChange={setPagination}
-				state={{ pagination, isLoading }}
-				renderDetailPanel={({ row }) =>
-					row.original.id && (
-						<ProfessionBusinessTypes
-							professionId={row.original.id}
-							professionName={row.original.name}
-							businessDomainId={row.original.business_domain_id}
-						/>
-					)
-				}
-			/>
-		</MainLayout>
-	);
+  return (
+    <MainLayout title="Profesii" hideAction>
+      <Table<ProfessionType>
+        data={data?.results}
+        rowCount={data?.count}
+        columns={columns}
+        manualPagination
+        onCreatingRowSave={onCreatingRowSave}
+        onEditingRowSave={onEditingRowSave}
+        onDeletingRowSave={onDeletingRowSave}
+        onPaginationChange={setPagination}
+        state={{ pagination, isLoading }}
+      />
+    </MainLayout>
+  );
 }
