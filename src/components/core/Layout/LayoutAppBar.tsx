2a2496bfdb45f005beb110ad38f292cd1c7eec86
raducubalgiu <balgiu_radu_2007@yahoo.com>
2026-03-14 10:01:27 +0200

    Create a separate component for ThemeToggle

--- src/components/core/Layout/LayoutAppBar.tsx
+++ src/components/core/Layout/LayoutAppBar.tsx
@@ -1,26 +1,10 @@
 import React from "react";
-import {
-  AppBar,
-  Toolbar,
-  Box,
-  IconButton,
-  Tooltip,
-  Menu,
-  Typography,
-  Divider,
-  MenuItem,
-  ListItemIcon,
-  ListItemText,
-} from "@mui/material";
+import { AppBar, Toolbar, Box, IconButton } from "@mui/material";
 import CustomStack from "../CustomStack/CustomStack";
-import DarkModeIcon from "@mui/icons-material/DarkMode";
-import LightModeIcon from "@mui/icons-material/LightMode";
 import MenuIcon from "@mui/icons-material/Menu";
 import LayoutSearch from "./LayoutSearch";
 import LayoutNotificationsMenu from "./LayoutNotificationsMenu";
-import { useThemeMode } from "@/providers/ThemeContext";
-import { ThemeModeEnum } from "@/providers/ThemeModeEnum";
-import SettingsBrightnessIcon from "@mui/icons-material/SettingsBrightness";
+import LayoutThemeToggle from "./LayoutThemeToggle";
 
 type LayoutAppBarProps = {
   onDrawerToggle: () => void;
@@ -31,35 +15,11 @@ export default function LayoutAppBar({
   onDrawerToggle,
   drawerWidth,
 }: LayoutAppBarProps) {
-  const { mode, setMode } = useThemeMode();
-
   const styles = {
     appBar: {
       width: { sm: `calc(100% - ${drawerWidth}px)` },
       ml: { sm: `${drawerWidth}px` },
     },
-  };
-
-  const [themeMenuAnchorEl, setThemeMenuAnchorEl] =
-    React.useState<null | HTMLElement>(null);
-  const isThemeMenuOpen = Boolean(themeMenuAnchorEl);
-
-  const handleThemeMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-    setThemeMenuAnchorEl(event.currentTarget);
-  };
-
-  const handleThemeMenuClose = () => {
-    setThemeMenuAnchorEl(null);
-  };
-
-  const handleThemeSelect = (themeMode: ThemeModeEnum) => {
-    setMode(themeMode);
-    handleThemeMenuClose();
-  };
-
-  const getThemeIcon = () => {
-    if (mode === ThemeModeEnum.SYSTEM) return <SettingsBrightnessIcon />;
-    return mode === ThemeModeEnum.LIGHT ? <LightModeIcon /> : <DarkModeIcon />;
   };
 
   return (
@@ -79,80 +39,7 @@ export default function LayoutAppBar({
           <LayoutSearch />
           <CustomStack>
             <LayoutNotificationsMenu />
-            <Tooltip title="Tema aplicatiei">
-              <IconButton
-                onClick={handleThemeMenuOpen}
-                sx={{
-                  mr: 1.5,
-                  borderRadius: 3,
-                  border: (theme) => `1px solid ${theme.palette.divider}`,
-                  backgroundColor: isThemeMenuOpen
-                    ? "action.selected"
-                    : "transparent",
-                  "&:hover": {
-                    backgroundColor: "action.hover",
-                  },
-                }}
-              >
-                {getThemeIcon()}
-              </IconButton>
-            </Tooltip>
-            <Menu
-              anchorEl={themeMenuAnchorEl}
-              open={isThemeMenuOpen}
-              onClose={handleThemeMenuClose}
-              anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-              transformOrigin={{ vertical: "top", horizontal: "right" }}
-              slotProps={{
-                paper: {
-                  sx: {
-                    mt: 1,
-                    minWidth: 300,
-                    borderRadius: 5,
-                  },
-                },
-              }}
-            >
-              <Box sx={{ px: 2, py: 1.25 }}>
-                <Typography
-                  variant="subtitle1"
-                  color="text.secondary"
-                  sx={{ fontWeight: 700 }}
-                >
-                  Tema aplicatiei
-                </Typography>
-              </Box>
-              <Divider />
-              <Box sx={{ py: 1 }}>
-                <MenuItem
-                  selected={mode === ThemeModeEnum.SYSTEM}
-                  onClick={() => handleThemeSelect(ThemeModeEnum.SYSTEM)}
-                >
-                  <ListItemIcon>
-                    <SettingsBrightnessIcon />
-                  </ListItemIcon>
-                  <ListItemText>SYSTEM</ListItemText>
-                </MenuItem>
-                <MenuItem
-                  selected={mode === ThemeModeEnum.LIGHT}
-                  onClick={() => handleThemeSelect(ThemeModeEnum.LIGHT)}
-                >
-                  <ListItemIcon>
-                    <LightModeIcon />
-                  </ListItemIcon>
-                  <ListItemText>LIGHT</ListItemText>
-                </MenuItem>
-                <MenuItem
-                  selected={mode === ThemeModeEnum.DARK}
-                  onClick={() => handleThemeSelect(ThemeModeEnum.DARK)}
-                >
-                  <ListItemIcon>
-                    <DarkModeIcon />
-                  </ListItemIcon>
-                  <ListItemText>DARK</ListItemText>
-                </MenuItem>
-              </Box>
-            </Menu>
+            <LayoutThemeToggle />
           </CustomStack>
         </CustomStack>
       </Toolbar>
---
+++ src/components/core/Layout/LayoutThemeToggle.tsx
@@ -0,0 +1,124 @@
+import { ThemeModeEnum } from "@/providers/ThemeModeEnum";
+import {
+  Box,
+  Divider,
+  IconButton,
+  ListItemIcon,
+  ListItemText,
+  Menu,
+  MenuItem,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import { useThemeMode } from "@/providers/ThemeContext";
+import SettingsBrightnessIcon from "@mui/icons-material/SettingsBrightness";
+
+const LayoutThemeToggle = () => {
+  const { mode, setMode } = useThemeMode();
+
+  const [themeMenuAnchorEl, setThemeMenuAnchorEl] =
+    React.useState<null | HTMLElement>(null);
+  const isThemeMenuOpen = Boolean(themeMenuAnchorEl);
+
+  const handleThemeMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setThemeMenuAnchorEl(event.currentTarget);
+  };
+
+  const handleThemeMenuClose = () => {
+    setThemeMenuAnchorEl(null);
+  };
+
+  const handleThemeSelect = (themeMode: ThemeModeEnum) => {
+    setMode(themeMode);
+    handleThemeMenuClose();
+  };
+
+  const getThemeIcon = () => {
+    if (mode === ThemeModeEnum.SYSTEM) return <SettingsBrightnessIcon />;
+    return mode === ThemeModeEnum.LIGHT ? <LightModeIcon /> : <DarkModeIcon />;
+  };
+
+  return (
+    <div>
+      <Tooltip title="Tema aplicatiei">
+        <IconButton
+          onClick={handleThemeMenuOpen}
+          sx={{
+            mr: 1.5,
+            borderRadius: 3,
+            border: (theme) => `1px solid ${theme.palette.divider}`,
+            backgroundColor: isThemeMenuOpen
+              ? "action.selected"
+              : "transparent",
+            "&:hover": {
+              backgroundColor: "action.hover",
+            },
+          }}
+        >
+          {getThemeIcon()}
+        </IconButton>
+      </Tooltip>
+      <Menu
+        anchorEl={themeMenuAnchorEl}
+        open={isThemeMenuOpen}
+        onClose={handleThemeMenuClose}
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+        transformOrigin={{ vertical: "top", horizontal: "right" }}
+        slotProps={{
+          paper: {
+            sx: {
+              mt: 1,
+              minWidth: 300,
+              borderRadius: 5,
+            },
+          },
+        }}
+      >
+        <Box sx={{ px: 2, py: 1.25 }}>
+          <Typography
+            variant="subtitle1"
+            color="text.secondary"
+            sx={{ fontWeight: 700 }}
+          >
+            Tema aplicatiei
+          </Typography>
+        </Box>
+        <Divider />
+        <Box sx={{ py: 1 }}>
+          <MenuItem
+            selected={mode === ThemeModeEnum.SYSTEM}
+            onClick={() => handleThemeSelect(ThemeModeEnum.SYSTEM)}
+          >
+            <ListItemIcon>
+              <SettingsBrightnessIcon />
+            </ListItemIcon>
+            <ListItemText>SYSTEM</ListItemText>
+          </MenuItem>
+          <MenuItem
+            selected={mode === ThemeModeEnum.LIGHT}
+            onClick={() => handleThemeSelect(ThemeModeEnum.LIGHT)}
+          >
+            <ListItemIcon>
+              <LightModeIcon />
+            </ListItemIcon>
+            <ListItemText>LIGHT</ListItemText>
+          </MenuItem>
+          <MenuItem
+            selected={mode === ThemeModeEnum.DARK}
+            onClick={() => handleThemeSelect(ThemeModeEnum.DARK)}
+          >
+            <ListItemIcon>
+              <DarkModeIcon />
+            </ListItemIcon>
+            <ListItemText>DARK</ListItemText>
+          </MenuItem>
+        </Box>
+      </Menu>
+    </div>
+  );
+};
+
+export default LayoutThemeToggle;
